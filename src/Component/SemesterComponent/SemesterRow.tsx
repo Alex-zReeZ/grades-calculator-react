8b14720895b873e277e762d2c7cc9de95db39e0f
Alex-zReeZ <alexbeck220@gmail.com>
2024-01-15 16:00:29 +0100

    add: when a grade is added, it is showed in a span with the right svg

--- src/Component/SemesterComponent/SemesterRow.tsx
+++ src/Component/SemesterComponent/SemesterRow.tsx
@@ -1,38 +1,30 @@
-import InputButton from "./InputButton.tsx"
+import InputButton from "./InputButton.tsx";
 import GradeElement from "./GradeComponent/GradeElement.tsx";
-import SemesterAverage from "./SemesterAverage.tsx"
+import SemesterAverage from "./SemesterAverage.tsx";
 import semesterIncrementation from "./semesterIncrementation.tsx";
-import {useState} from "react";
-
+import { useState } from "react";
 
 export default function SemesterRow() {
+    const [allGrades, setGrades] = useState<number[]>([]);
 
-    const [allGrades,setGrades] = useState<number[]>([])
+    const addGradeToSemester = (g: number) => setGrades((grades) => [...grades, g]);
 
-    console.clear()
-    console.log(allGrades)
-    const addGradeToSemester = (g: number) => setGrades(grades => [...grades, g])
+    const gradeWithColor = allGrades.map((grade, index) => (
+        <GradeElement key={index} grade={grade} />
+    ));
 
     return (
         <>
-            <div
-                className="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-0"
-            >
+            <div className="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-0">
                 {semesterIncrementation()}
-                <dd
-                    className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-4 sm:mt-0 flex justify-between gap-x-1.5"
-                >
-                    <div
-                        className="flex flex-row flex-nowrap overflow-y-scroll gap-x-1.5"
-                    >
-                        {allGrades}
-                        <GradeElement grade={5}/>
-                        <InputButton onNewGradeAdded={addGradeToSemester}/>
+                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-4 sm:mt-0 flex justify-between gap-x-1.5">
+                    <div className="flex flex-row flex-nowrap overflow-y-scroll gap-x-1.5">
+                        {gradeWithColor}
+                        <InputButton onNewGradeAdded={addGradeToSemester} />
                         <SemesterAverage average={5} />
                     </div>
                 </dd>
             </div>
-
         </>
-    )
+    );
 }
